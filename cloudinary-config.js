02ce942a6677d733981d98d2f41c87e546c3bfa9
dmytro-marchuk <darthwudoodoodoo@gmail.com>
2023-11-04 13:31:18 +0000

    cloudinary config

--- cloudinary-config.js
+++ cloudinary-config.js
@@ -1,7 +1,17 @@
-import { config } from "cloudinary-react";
+// import { config } from "cloudinary-react";
 
-config({
+// config({
+//   cloudName: "dindon",
+//   apiKey: "517767964973866",
+//   apiSecret: "kZwW7WJSawyv1-Ifts0TViwfzxg",
+// });
+
+import cloudinary from "cloudinary-core";
+
+const cloudinaryInstance = new cloudinary.Cloudinary({
   cloudName: "dindon",
   apiKey: "517767964973866",
   apiSecret: "kZwW7WJSawyv1-Ifts0TViwfzxg",
 });
+
+export default cloudinaryInstance;
