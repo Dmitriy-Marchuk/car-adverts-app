cda6c9bb6ba1729acca2993eb373fde20c408210
dmytro-marchuk <darthwudoodoodoo@gmail.com>
2023-11-07 21:43:19 +0000

    fetch changed to axios

--- src/services/getApi.js
+++ src/services/getApi.js
@@ -1,18 +1,21 @@
+import axios from "axios";
+
 const API_KEY = "6543df3801b5e279de211518";
+const url = `https://${API_KEY}.mockapi.io/api/adverts`;
 
 const getFetchCollection = async (page, perPage) => {
-  const url = new URL(`https://${API_KEY}.mockapi.io/api/adverts`);
-  url.searchParams.append("page", page);
-  url.searchParams.append("limit", perPage);
-
-  return await fetch(url, {
-    method: "GET",
-  })
-    .then((response) => response.json())
-    .then((data) => data)
-    .catch((error) => {
-      throw new Error(error);
+  try {
+    const response = await axios.get(url, {
+      params: {
+        page: page,
+        limit: perPage,
+      },
     });
+
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 export default getFetchCollection;
