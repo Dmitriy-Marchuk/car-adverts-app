e98a9cdc6f0bce1d7b3a12670038bc5339a5156d
dmytro-marchuk <darthwudoodoodoo@gmail.com>
2023-11-07 22:05:38 +0000

    adaptive add

--- src/Components/CarCollection/CarCollection.styled.js
+++ src/Components/CarCollection/CarCollection.styled.js
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 
 export const CarCollectionWrapper = styled.ul`
+  @media (min-width: 768px) {
+    max-width: calc(100vw - 256px);
+  }
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-template-columns: repeat(auto-fill, minmax(274px, 1fr));
   grid-column-gap: 29px;
   grid-row-gap: 50px;
-  max-width: calc(100vw - 256px);
   padding: 50px 0;
 `;
--- src/styles/common/general.styled.js
+++ src/styles/common/general.styled.js
@@ -2,4 +2,7 @@ import { styled } from "styled-components";
 
 export const Section = styled.section`
   padding: 0 128px;
+  @media (max-width: 768px) {
+    padding: 0;
+  }
 `;
